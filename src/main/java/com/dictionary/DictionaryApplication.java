986c1c188fdf57b67611f9574a4e7c6c65e5343c
Niranjan chavadi <56150400+niranjanchavadi@users.noreply.github.com>
2024-09-14 23:22:01 +0530

    Update DictionaryApplication.java  removed unsused method and objects

--- src/main/java/com/dictionary/DictionaryApplication.java
+++ src/main/java/com/dictionary/DictionaryApplication.java
@@ -1,34 +1,15 @@
 package com.dictionary;
 
-import com.dictionary.service.CommandHandler;
-import com.dictionary.service.DictionaryApiClient;
-import com.dictionary.service.WordGame;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.IOException;
 
 @SpringBootApplication
 public class DictionaryApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
-
-
-
-        DictionaryApiClient apiClient = new DictionaryApiClient(); // Assuming ApiClient has a no-arg constructor
-        WordGame wordGame = new WordGame(apiClient, new CommandHandler()); // Pass dependencies to WordGame
-
-        // Create the CommandHandler with dependencies
-        CommandHandler handler = new CommandHandler(wordGame,apiClient);
-
-    try {
-        handler.handleCommand(args);
-    }
-catch (IOException e){
-       e.printStackTrace();
     }
 
 
-}
 }
